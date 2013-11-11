2abd15de5463ae90bf5c55fb4a6af82cfbac232a
Adrian Banks <adrian@grantadesign.com>
2013-11-11 12:20:15 +0000

    First test

---
+++ TeaParty/GuestGreeter.cs
@@ -0,0 +1,10 @@
+﻿namespace TeaParty
+{
+    public class GuestGreeter
+    {
+        public string Greet(string surname, bool isMale)
+        {
+            return "Hello Ms. Austen";
+        }
+    }
+}
---
+++ TeaParty/GuestGreeterTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TeaParty
+{
+    [TestFixture]
+    public sealed class GuestGreeterTests
+    {
+        [Test]
+        public void AWomanIsGreetedAsMs()
+        {
+            var guestGreeter = new GuestGreeter();
+            guestGreeter.Greet("Austen", false);
+        }
+    }
+}
