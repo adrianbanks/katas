1e169e29282c70be9da6036d2278bd966ca7cb28
Adrian Banks <adrian@grantadesign.com>
2013-11-11 12:22:53 +0000

    Another test

--- TeaParty/GuestGreeterTests.cs
+++ TeaParty/GuestGreeterTests.cs
@@ -5,12 +5,13 @@ namespace TeaParty
     [TestFixture]
     public sealed class GuestGreeterTests
     {
-        [Test]
-        public void AWomanIsGreetedAsMs()
+        [TestCase("Austen")]
+        [TestCase("Jones")]
+        public void AWomanIsGreetedAsMs(string surname)
         {
             var guestGreeter = new GuestGreeter();
-            string greeting = guestGreeter.Greet("Austen", false);
-            Assert.That(greeting, Is.EqualTo("Hello Ms. Austen"));
+            string greeting = guestGreeter.Greet(surname, false);
+            Assert.That(greeting, Is.EqualTo("Hello Ms. " + surname));
         }
 
         [TestCase("Orwell")]
